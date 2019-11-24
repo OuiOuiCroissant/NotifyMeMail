7f5f9ce9b13117cd2e59b1d84eb7a9b61bfae1c5
OuiOuiCroissant <50111424+OuiOuiCroissant@users.noreply.github.com>
2019-11-24 02:14:46 +0200

    fixed mongodb

--- mongo-database/find_user.js
+++ mongo-database/find_user.js
@@ -2,11 +2,11 @@ module.exports = function findUser(username) {
   const mongoose = require('mongoose');
   const user_schema = require('./user_schema.js');
 
-  const Client = mongoose.model('Client', user_schema, 'Client');
+  const User = mongoose.model('User', user_schema, 'Users');
 
   mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
 
-  Client.findOne({ username: username }, (err, res) => {
+  User.findOne({ username: username }, (err, res) => {
     if (err) throw err;
     if (res) return telegramId;
   });
